f17c1a94226af2e85990207c6ecc9eaa7afb739f
ducva92 <ducva92@gmail.com>
2020-07-30 21:55:28 +0900

    Update Image.js
    ưqe

--- src/components/main/Image.js
+++ src/components/main/Image.js
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TableInfo from "./Table";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import { Paper } from "@material-ui/core";
 import SingleLineGridList from "./ImageList";
 
