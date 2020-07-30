92b71207c86cc7cff47af800c75d6a56ed3250eb
Guava <linh.guava94@gmail.com>
2020-07-30 14:24:19 +0900

    chưa fix được lỗi ảnh trong ViewList

--- src/components/main/ViewList.js
+++ src/components/main/ViewList.js
@@ -1,5 +1,89 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import CardHeader from "@material-ui/core/CardHeader";
+import QueryBuilder from "@material-ui/icons/QueryBuilder";
+import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+
+
+const useStyles = makeStyles({
+  root: {
+    flexFlow: 1,
+    margin: "30px 30px",
+  },
+  card: {
+    padding: "10px",
+    maxWidth: "300px",
+  },
+  media: {
+    maxHeight: "150px",
+  },
+  header: {
+    margin: "-15px 0 -10px 0",
+  },
+  time: {
+    marginRight: "10px",
+    opacity: "0.5",
+    fontSize: "small",
+  },
+  icon: {
+    marginRight: "10px",
+    fontSize: "12px",
+  },
+  description: {
+    fontWeight: "bold",
+    marginLeft: "-10px",
+    fontSize: "12px",
+    whiteSpace: "nowrap",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+  },
+});
+
+function getImgPath(imgSrc) {
+  return require("../../" + imgSrc);
+}
 
 export default function ViewList(props) {
-  return <div></div>;
+  const classes = useStyles();
+
+  return (
+    <section className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <div className={classes.content}>
+            <h3>{props.data.heading}</h3>
+            {props.data.paragraphs.map((para, index) => {
+              return (
+                <article key={index}>
+                  <CardHeader className={classes.header} title={props.data.title} />
+                  <CardMedia
+                    component="img"
+                    className={classes.media}
+                    src={getImgPath(para.image.src)}
+                    title={para.title}
+                  />
+                  <CardContent className={classes.content}>
+                    <Typography
+                      className={classes.description}
+                      variant="caption"
+                      color="textSecondary"
+                      component="p"
+                      children
+                    >
+                      {para.description}
+                    </Typography>
+                  </CardContent>
+                  </article>
+               );
+            })}
+          </div>
+        </Grid>
+      </Grid>
+    </section>
+  );
 }
--- src/data/recruit.js
+++ src/data/recruit.js
@@ -28,12 +28,28 @@ const RecruitJSON = {
     {
       type: "ViewList",
       heading: "Cam nhan cua nguoi cong ty",
-      items: [
-        {
-          image: { src: "", alt: "" },
-          heading: "",
-          content: "",
-        },
+      paragraphs: [
+          {
+            image: { src: "anh1.jfif", alt: "" },
+            title: "ABC",
+            description:
+              "未経験で不安だったけど、周りの先輩たちの指導のおかげで毎日充実しています。私はウェブサイト作成の実務経験がなかなかやりたい仕事を見つけるのが難しいと思います。最初は、未経験者であることから不安もありましたが、周りの先輩が丁寧、親身になって教えて頂くので安心して働いています。あと、自分から頑張って、どんどん技術を身に着けて、毎日が充実しています。",
+            
+          },
+          {
+            image: { src: "anh1.jfif", alt: "" },
+            title:"DFJ",
+            description:
+              "これから自分が働く職場は一体何をしているのか、という事を実際に手を動かして体験できてよかったです。自分の現段階のスキルや、自分に足りないところの認識もできました。全部署の仕事を、実際に体験させてもらうことで、入社してから自分が一体何をしていきたいのか？を再確認できた有意義な時間でした。",
+            
+          },
+          {
+            image: { src: "anh1.jfif", alt: "" },
+            title:"GH",
+            description:
+              "aaaa",
+            
+          },
       ],
     },
   ],
