6c71d30103f3a9fc23c85049410b39266931a3a4
vuduchuy94 <vuduchuybk@gmail.com>
2020-07-30 11:19:09 +0900

    edited 応募フォーム

--- src/data/recruit.js
+++ src/data/recruit.js
@@ -36,6 +36,64 @@ const RecruitJSON = {
         },
       ],
     },
+    {
+      type:"Form",
+      action: "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfGnBVVx08IipMbyW8ZKKhh-RirPIbxJKqHF0DJVnE1QWCIOw/formResponse",
+      items:[
+        {
+          type:"text",
+          name:"entry.1781500597",
+          placeholder:"名前"
+        },
+
+        {
+          type:"text",
+          name:"entry.1640447617",
+          placeholder:"性別"
+        },
+
+        {
+          type:"text",
+          name:"entry.297979220",
+          placeholder:"生年月日"
+        },
+
+        {
+          type:"text",
+          name:"entry.1078004677",
+          placeholder:"住所"
+        },
+
+        {
+          type:"text",
+          name:"entry.2040870261",
+          placeholder:"メール"
+        },
+
+        {
+          type:"text",
+          name:"entry.1916706626",
+          placeholder:"電話番号"
+        },
+
+        {
+          type:"text",
+          name:"entry.324267323",
+          placeholder:"大学名、学科"
+        },
+
+        {
+          type:"text",
+          name:"entry.340482159",
+          placeholder:"日本語レベル"
+        },
+        {
+          type:"text",
+          name:"entry.86165619",
+          placeholder:"職歴"
+        }
+              ]
+    },
   ],
 };
 
