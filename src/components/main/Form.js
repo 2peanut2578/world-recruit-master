040550fd2566b36170acaf6ed7d3ad279ac4fb56
minhloc99 <68985225+minhloc99@users.noreply.github.com>
2020-07-31 12:56:46 +0900

    Update Form.js

--- src/components/main/Form.js
+++ src/components/main/Form.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./Form.css";
 import Heading from "./Heading";
+import Background from "../../img/bg.jpg";
 export default function Form(props) {
   return (
     <div className="wraper">
-      <div>
-        <Heading data={props.data} />
+      <Heading data={props.data} />
 
-        <form action={props.data.action} className="contact-box">
-          <div className="left"></div>
-          <div className="right">
+      <div className="contact-box">
+        <div className="left">
+          <img src={Background} alt="" />
+        </div>
+        <div className="right">
+          <form action={props.data.action}>
             {props.data.items.map((item, index) => {
               return (
                 <div>
@@ -29,8 +32,8 @@ export default function Form(props) {
               className="field"
             ></textarea>
             <input type="submit" value="応募" className="btn" />
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   );
