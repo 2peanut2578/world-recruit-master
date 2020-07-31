72c4d82069fdbaac7e4acadc38fc9cbd215e183a
lamvien-2020 <68042774+lamvien-2020@users.noreply.github.com>
2020-07-31 12:15:31 +0900

    Update TextList.js
    update css TestList

--- src/components/main/TextList.js
+++ src/components/main/TextList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import Heading from "./Heading";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,33 +14,37 @@ const useStyles = makeStyles((theme) => ({
     width: "60%",
   },
   block: {
-    padding: "10px",
+    padding: "5px 5px 5px 5px",
     width: "100%",
-    "& h3": {
-      fontSize: "13px",
-      color: "#374048",
-    },
+    textAlign: "justify",
     "& p": {
-      fontSize: "12px",
+      fontSize: "14px",
       color: "#737c85",
     },
   },
-  content: {
-    margin: "auto",
-    maxWidth: "700px",
-    paddingLeft: "20px",
-    paddingRight: "20px",
-    "& ul": {
-      margin: 0,
-      padding: 10,
-      "& li": {
-        padding: 10,
-        paddingLeft: "50px",
-        textAlign: "left",
-        listStyleType: "circle",
-        listStylePosition: "inside",
-      },
+  subtitle: {
+    position: "relative",
+    fontSize: "12px",
+    padding: "-20px 0 50px 0",
+    color: `rgba(0,0,0,0.6)`,
+    "&:after": {
+      content: `' '`,
+      position: "absolute",
+      bottom: -5,
+      left: "30%",
+      width: "40%",
+      height: "1px",
+      background: "#A0A6AF",
     },
+  },
+  reason: {
+    position: "relative",
+    fontSize: "15px",
+    textAlign: "justify",
+    color: `rgba(0,0,0,0.6)`,
+  },
+  content:{
+    padding: "10px",
   },
 }));
 
@@ -50,18 +54,27 @@ export default function TextList(props) {
   return (
     <section className={classes.root}>
       <div className={classes.wrapper}>
-        <Heading data={props.data} />
-        <Grid container spacing={1}>
+        <Typography variant="title">
+          <strong>
+            <h3>{props.data.title}</h3>
+          </strong>
+          <p className={classes.subtitle}>{props.data.subtitle}</p>
+        </Typography>
+        <Grid container spacing={1} className={classes.content}>
           {props.data.items.map((item, index) => {
             return (
               <Grid item xs={6}>
-                <div className={classes.block}>
-                  <h3>{item.heading}</h3>
-                  <p>{item.content}</p>
+                <div className={classes.block}>                  
+                  <p>{item.para}</p>
                 </div>
               </Grid>
             );
           })}
+        </Grid>
+        <Grid justify="center" item xs={12} sm={12}>
+          <Typography variant="body1">
+            <p className={classes.reason}>{props.data.reason}</p>
+          </Typography>
         </Grid>
       </div>
     </section>
