6065c93473affa4a1a2a80c6fb99b8b5996bbe5e
vusinh2012 <62141792+vusinh2012@users.noreply.github.com>
2020-07-31 14:22:32 +0900

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,9 @@
 import React from "react";
-import { slide as Menu } from 'react-burger-menu'
-import { makeStyles} from '@material-ui/core/styles';
-import Grid from "@material-ui/core/Grid";
-import ButtonBase from '@material-ui/core/ButtonBase';
+import { makeStyles } from "@material-ui/core/styles";
+import { HomeOutlined, DetailsOutlined, GroupWorkOutlined, RecordVoiceOverOutlined, BusinessCenterOutlined, MenuOpenRounded} from '@material-ui/icons';
+import Background from "../../img/background.png"
 import IconButton from '@material-ui/core/IconButton';
 import AppBar from '@material-ui/core/AppBar';
-import { HomeOutlined, DetailsOutlined, GroupWorkOutlined, RecordVoiceOverOutlined, BusinessCenterOutlined, MenuOpenRounded} from '@material-ui/icons';
-
-import Background from "../../img/background.png"
 import Logo from "../../img/logo.png"
 
 const useStyles = makeStyles((theme) => ({
@@ -21,172 +17,170 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: "45%",
     position: "relative",
   },
-  container: {
-    position: "absolute",
-    maxWidth: '100%',
-    backgroundColor: `rgba(0,0,0,0.2)`,
+  header: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        padding: "0 100px",
+        width: "100%",
+        boxSizing: "border-box",
+        background: `rgba(0,0,0,.3)`,
+        '& nav': {
+            float: "right",
+            marginRight: "-100px",
+            '& ul': {
+                margin: 0,
+                padding: 0,
+                display: "flex",
+                '& li': {
+                    listStyle: "none",
+                    '& a': {
+                        position: "relative",
+                        height: "50px",
+                        lineHeight: "50px",
+                        margin: "10px 0px",
+                        padding: "0 20px",
+                        color: "#fff",
+                        textDecoration: "none",
+                        display: "block",
+                        textShadow: `0 2px 5px rgba(0,0,0,.5)`,
+                        fontWeight: "bold",
+                        '&::before': {
+                            content: `' '`,
+                            position: "absolute",
+                            bottom: "5px",
+                            left: "10px",
+                            width: "100%",
+                            height: "5px",
+                            background: "#A3C6FA",
+                            zIndex: "-1",
+                            transform: `scaleX(0)`,
+                            transformOrigin: "left",
+                            transition: `transform 0.5s`,
+                            zIndex: 999
+                        },
+                        '&:hover::before' : {
+                            transform: `scaleX(1)`,
+                            zIndex: 999
+                        }
+                    }
+                }
+            }
+        },
+  }, 
+  [theme.breakpoints.down('sm')]: {
+        header: {
+            padding: "0 20px",
+            '& nav': {
+                position: "absolute",
+                width: "100%",
+                height: `calc(100vh - 50px)`,
+                background: `rgba(0,0,0,.2)`,
+                top: "72px",
+                left: "-100%",
+                transition: 0.5,
+                '&.active': {
+                    left: 0
+                },
+                '& ul': {
+                    display: "block",
+                    textAlign: "center",
+                    '& li': {
+                        '& a': {
+                            position: "relative",
+                            margin: "20px 0px 20px 0",
+                            textAlign: "right",
+                            '&::before': {
+                                content: `' '`,
+                                position: "absolute",
+                                bottom: "5px",
+                                left: "75%",
+                                width: "35%",
+                                height: "5px",
+                                background: "#A3C6FA",
+                                zIndex: "-1",
+                                transform: `scaleX(0)`,
+                                transformOrigin: "left",
+                                transition: `transform 0.5s`,
+                                zIndex: 999
+                            },
+                            '&:hover::before' : {
+                                transform: `scaleX(1)`,
+                                zIndex: 999
+                            }
+                        }
+                    }
+                }
+            }
+        }
   },
-  topNav: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      overflow: "hidden",
-      display: 'flex',
-    },
-    '& a': {
-      position: "relative",
-      padding: "14px 20px",
-      color: "#fff",
-      textDecoration: "none",
-      fontSize: "20px",
-      '&::before': {
-          content: `' '`,
-          position: "absolute",
-          bottom: "5px",
-          left: "20px",
-          width: "70%",
-          height: "5px",
-          background: "#388CFB",
-          zIndex: "-1",
-          transform: `scaleX(0)`,
-          transformOrigin: "left",
-          transition: `transform 0.5s`,
-      },
-      '&:hover::before' : {
-        transform: `scaleX(1)`,
-      },
-    }
+
+  activeItem: {
+      left: 0
   },
   logo: {
-    display: 'block',
-    margin: "10px",
-    maxWidth: '100%',
-    maxHeight: '100%',
-    
-  },
-  menuButton: {
-    [theme.breakpoints.up('sm')]: {
-      display: 'none',
+    margin: "10px 0 0px -80px",
+    float: "left",
+    [theme.breakpoints.down('sm')]: {
+        margin: "10px 10px 0 10px"
     },
-  },
-  toggle: {
-    color: "#fff",
-    fontSize: "50px",
-  },
-  sidebar: {
-      width: "auto",
-      maxWidth: "100%",
-      maxHeight: "100%",
-      overflow: "auto",
-      border: "none",
-      '& ul': {
-          margin: 0,
-          padding: 0,
-        '& li': {
-          textAlign: "center",
-          listStyleType: "none",
-          '& a': {
-              position: "relative",
-              color: "#fff",
-              fontSize: "28px",
-              textDecoration: "none",
-              margin: "30px 0px",
-              display: "block",
-              textShadow: `0 2px 5px rgba(0,0,0,.5)`,
-              '&::before': {
-                  content: `' '`,
-                  position: "absolute",
-                  bottom: "0px",
-                  left: "25%",
-                  width: "50%",
-                  height: "5px",
-                  background: "#388CFB",
-                  zIndex: "-1",
-                  transform: `scaleX(0)`,
-                  transformOrigin: "left",
-                  transition: `transform 0.5s`,
-              },
-              '&:hover::before' : {
-                  transform: `scaleX(1)`,
-              }
-          }
-        }
-      },
-      [theme.breakpoints.up('sm')]: {
-        display: "none",
-      },
-  },
-  dialog: {
-    [theme.breakpoints.up('sm')]: {
-      display: "none",
-    },
-  },
+  },  
   icon: {
-    margin: "-4px 5px -4px 0px"
+    margin: "-4px 5px -4px 0px",
+    [theme.breakpoints.down('sm')]: {
+        display: "none",
+    },
+  },  
+  toggleButton: {
+    position: "fixed",
+    right: "1%",
+    top: "2%",
+    padding: "5px",
+    cursor: "pointer",
+    fontSize: "45px",
+    display: "none",
+    zIndex: 999,
+    [theme.breakpoints.down('sm')]: {
+        display: "flex",
+        zIndex: 999,
+    },
   }
 }));
 
+function show (){
+    let element = document.getElementById("sidebar")
+    element.classList.toggle("active")
+}
+
 function Header() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-
   return (
     <React.Fragment>
-      <AppBar 
+        <AppBar 
             position="sticky"
             color = "transparent"
-      >
-        <Grid container className={classes.container}>
-            <Grid item xs container justify="flex-start" alignItems="center">
-              <ButtonBase>
-                <img className={classes.logo} alt="complex" src={Logo} />
-              </ButtonBase>
-            </Grid>
-            <Grid item xs={9} container direction="row" justify="flex-end" alignItems="center" className={classes.topNav}>
-                <a href="#home"><HomeOutlined className={classes.icon}/>Home</a>
-                <a href="#corporate"><GroupWorkOutlined className={classes.icon} />Corporate</a>
-                <a href="#about"><DetailsOutlined className={classes.icon} />About</a>
-                <a href="#interview"><RecordVoiceOverOutlined className={classes.icon} />Interview</a>
-                <a href="#entry"><BusinessCenterOutlined className={classes.icon} />Entry</a>
-            </Grid>
-            <Grid item xs className={classes.menuButton}>
-                <IconButton
-                    edge = "end"
-                    selected={open}
-                    onChange={() => {
-                        setOpen(!open);
-                    }}
-                >
-                    <MenuOpenRounded  className= {classes.toggle} />
-                </IconButton>
-            
-                <Grid container>
-                    <Grid item xs>
-                        <Menu 
-                            width={ '50%' } 
-                            right 
-                            disableCloseOnEsc 
-                            disableOverlayClick
-                            isOpen={open}  
-                            onStateChange={() => { setOpen(!open);}} 
-                            className={classes.dialog}
-                        >
-                            <div className={classes.sidebar}>
-                                <ul>
-                                    <li><a href="#home">Home</a></li>
-                                    <li><a href="#corporate">Corporate</a></li>
-                                    <li><a href="#about">About</a></li>
-                                    <li><a href="#interview">Interview</a></li>
-                                    <li><a href="#entry">Entry</a></li>
-                                </ul>
-                            </div>
-                        </Menu>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Grid>
-      </AppBar>
-      <div className={classes.bgImg}></div>
+        >
+            <section> 
+                <header className={classes.header}>
+                    <div className={classes.logo}><a href = "#"><img src={Logo}></img></a></div>
+                    <nav id = "sidebar" className = {classes.sidebar}>
+                        <ul>
+                            <li><a href="#home"><HomeOutlined className={classes.icon}/>Home</a></li>
+                            <li><a href="#company"><GroupWorkOutlined className={classes.icon} />Company</a></li>
+                            <li><a href="#about"><DetailsOutlined className={classes.icon} />About</a></li>
+                            <li><a href="#impression"><RecordVoiceOverOutlined className={classes.icon} />Impression</a></li>
+                            <li><a href="#entry"><BusinessCenterOutlined className={classes.icon} />Entry</a></li>
+                        </ul>
+                    </nav>
+                    <IconButton onClick={() => {show();}}>
+                            <MenuOpenRounded  color="primary" className= {classes.toggleButton} />
+                    </IconButton>
+                </header>
+            </section>
+    </AppBar>
+    <div className={classes.bgImg}></div>
     </React.Fragment>
   );
 }
+
 export default Header
